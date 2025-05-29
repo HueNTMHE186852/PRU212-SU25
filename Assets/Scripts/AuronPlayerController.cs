3fa99f779427fe1531f329b8b269cf10db328e90
Hoang Bao Hung (K18 HL) <hunghbhe187115@fpt.edu.vn>
2025-05-30 02:02:32 +0700

    jump and attack_1 for auron character

--- Assets/Scripts/AuronPlayerController.cs
+++ Assets/Scripts/AuronPlayerController.cs
@@ -6,10 +6,16 @@ public class AuronPlayerController : MonoBehaviour
 {
     private Animator animator;
     public float moveSpeed = 3f;
+    private Rigidbody2D rb;
+
+    public float jumpForce = 7f;
+    private bool isGrounded = true;
+    private bool isAttacking = false;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -21,6 +27,11 @@ public class AuronPlayerController : MonoBehaviour
 
         bool isMoving = movement.sqrMagnitude > 0f;
         animator.SetBool("IsMoving", isMoving);
+        if (Input.GetMouseButtonDown(0) && !isAttacking)
+        {
+            isAttacking = true;
+            animator.SetBool("IsAttacking", true);
+        }
 
         if (isMoving)
         {
@@ -36,5 +47,31 @@ public class AuronPlayerController : MonoBehaviour
                 transform.localScale = scale;
             }
         }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            animator.SetBool("IsJumping", false);
+
+            // Cập nhật IsMoving để đảm bảo chuyển trạng thái đúng sau khi tiếp đất
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            bool isMoving = (new Vector3(horizontal, 0, vertical)).sqrMagnitude > 0f;
+            animator.SetBool("IsMoving", isMoving);
+        }
+    }
+    public void EndAttack()
+    {
+        Debug.Log("EndAttack called");
+        isAttacking = false;
+        animator.SetBool("IsAttacking", false);
     }
 }
