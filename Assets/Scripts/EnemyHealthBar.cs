de2ad582c74d91689240520d5350ab5a43ef5ea3
AnLPVHE187047 <anlpvhe187047@fpt.edu.vn>
2025-06-09 22:22:24 +0700

    Create healthbar for enemy 2

---
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    public Image _thanhMau;
+    public void updateHeathBar(float currentHealth, float maxHealth)
+    {
+        _thanhMau.fillAmount = currentHealth / maxHealth;
+    }
+}
