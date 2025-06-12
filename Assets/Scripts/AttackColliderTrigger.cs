d1706b79223daa1314f9bf39f8f6f620ea464705
Hoang Bao Hung (K18 HL) <hunghbhe187115@fpt.edu.vn>
2025-06-13 00:10:51 +0700

    fix

---
+++ Assets/Scripts/AttackColliderTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AttackColliderTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("💥 Player bị trúng đòn từ AttackZone!");
+        }
+    }
+}
