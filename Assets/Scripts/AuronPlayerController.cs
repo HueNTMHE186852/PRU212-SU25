d4ff2e54200288d838e675ad7d78e4f1dd4fa164
Hoang Bao Hung (K18 HL) <hunghbhe187115@fpt.edu.vn>
2025-06-16 17:17:49 +0700

    arrow in new map

--- Assets/Scripts/AuronPlayerController.cs
+++ Assets/Scripts/AuronPlayerController.cs
@@ -89,7 +89,7 @@ public class AuronPlayerController : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Tilemap"))
         {
             isGrounded = true;
             animator.SetBool("IsJumping", false);
