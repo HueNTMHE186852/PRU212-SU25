f62ab7f810965c15927d131fac087ba7b06464e3
anhnhh2 <nhhagp2@gmail.com>
2025-07-22 09:47:35 +0700

    Update things

--- Assets/Scripts/Dragon/DyingState.cs
+++ Assets/Scripts/Dragon/DyingState.cs
@@ -6,17 +6,9 @@ public class DyingState : IDragonState
 
     public DyingState(DragonController ctrl) => controller = ctrl;
 
-    public void Enter(Player1 player1, AuronPlayerController player2)
+    public void Enter()
     {
         controller.animator.SetTrigger("Death");
-
-        if (GameProgress.Current.currentLevel != 4)
-        {
-                if (player1 != null)
-                    player1.Win();
-                if (player2 != null)
-                    player2.Win();
-        }
 
         GameObject.Destroy(controller.gameObject, 1.5f);
     }
